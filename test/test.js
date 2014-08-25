1481e7b45f5b6d1fce6f6bec06b680e79e958782
stanley zheng <dev.stanzheng@gmail.com>
2014-08-25 00:51:53 -0400

    adding more test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,24 @@
 var assert = require('chai').assert;
 var hangman = require('../lib/hangman');
 
-describe('Word Functionality', function(){
-
-  describe('PickWord', function(done){
-        it('Count Words', function(){
-          assert.notEqual(hangman.getWordList(), null,'Is Not Null');
+describe('Word Functionality', function(done){
+  var list = ["fo", "bar", "batz"];
+  describe('PickWord', function(){
+        it('Picks Words', function(){
+          assert.equal(hangman.pickWord(list,3), "bar",'Picks Bar');
           done();
+        });
+  });
+
+  describe('PickWord', function(){
+        it('Picks Words', function(){
+          var list = ["Foo", "Bar", "Batz"];
+          assert.equal(hangman.listToLower(list,3), ["fo", "bar", "batz"],'Picks Bar');
+        });
+  });
+  describe('Generate Word', function(){
+        it('Given Word', function(){
+          assert.equal(hangman.generateCLI("chicken",["b","c"]), " c _ _ c _ _ _",'Only show c');
         });
   });
 });
