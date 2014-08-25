dd54aff07eac2de552a67c9beddabaa48bc40ad6
stanley zheng <dev.stanzheng@gmail.com>
2014-08-25 11:06:33 -0400

    adding progress, #TODO trying to run procedural style while wanting not

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var hangman = require('./lib/hangman');
 var fs = require('fs');
 var readline = require('readline');
-//var _ = require('lodash');
+var _ = require('lodash');
 
+var render = function (chosenWord,letters) {
+  var new_letter;
+  var obj = hangman.generateCLI(chosenWord, letters);
+  console.log(obj.view);
+  console.log(obj);
+  new_letter = hangman.getLetter();
+  //check if letter is usable, then restart
+  setTimeout(function playround(){
+    if (!_.contains(letters,new_letter)){
+      console.log("Thanks! this letter was added: " + new_letter);
+      letters.push(new_letter);
+      render (chosenWord, letters);
+    }
+    else{
+      console.log("Sorry, this letter exists and was not added " + new_letter);
+      render (chosenWord, letters);
+    }
+  }, 5000);
+};
 
 (function playGame() {
-  var words, size;
+  var words, size, picked;
   words = fs.readFileSync('./assets/words.txt', 'utf-8');
   words = words.split('\n');
 
@@ -18,7 +37,8 @@ var readline = require('readline');
         size = parseInt(answer);
         if ((typeof size === 'number') && (!isNaN(size))){
           console.log("Starting Game for Word Size:", answer);
-          hangman.pickWord(words,size);
+          picked = hangman.pickWord(words,size);
+          render(picked, []);
           rl.close();
         }
         else {
